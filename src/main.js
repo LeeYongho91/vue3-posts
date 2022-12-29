42b88eee0218b24cc9c50c68db7d1d10d58e2f0a
LeeYongho91 <skyks4848@naver.com>
2022-12-29 19:42:06 +0900

    20221229

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from '@/router';
 
-import './assets/main.css'
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
