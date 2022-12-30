e190a28a25b2db1ecd3639f055a86c1824546058
LeeYongho91 <skyks4848@naver.com>
2022-12-30 16:40:15 +0900

    20221230

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -1,5 +1,6 @@
 <template>
-	<div class="card">
+	<AppCard>
+		<template v-slot:header>헤더</template>
 		<div class="card-body">
 			<h5 class="card-title">{{ title }}</h5>
 			<p class="card-text">
@@ -7,10 +8,12 @@
 			</p>
 			<p class="text-muted">{{ createdAt }}</p>
 		</div>
-	</div>
+		<template v-slot:footer>푸터</template>
+	</AppCard>
 </template>
 
 <script setup>
+import AppCard from '@/components/AppCard.vue';
 defineProps({
 	title: {
 		type: String,
